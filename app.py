2fe292cd783ce4346956f911085e8c99fcb54bcd
Ashwin M Nayak <ashwinnayak268@gmail.com>
2025-09-02 06:12:05 +0530

    commit - app.py
    Updated sidebar with analysis options in Streamlit app

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import streamlit as st
 
 df = pd.read_csv('startup_funding.csv')
 
-df['Investor Name'] = df['Investor Name'].fillna('Unknown')
-
 st.sidebar.title('Startup Funding Analysis Dashboard')
-option = st.sidebar.selectbox('select one')
+option = st.sidebar.selectbox('select one', ['Overall Analysis','StartUp', 'Investors'])
+if option == 'Overall Analysis':
+    st.title('Overall Analysis')
+elif option == 'StartUp':
+    st.sidebar.selectbox('select StartUp',sorted(df['Startup Name'].unique()))
+    btn1 = st.sidebar.button('Get StartUp Details')
+    st.title('Startup Analysis')
+else:
+    st.sidebar.selectbox('select Investor',sorted(df['Investors Name'].fillna("Unknown").unique()))
+    btn2= st.sidebar.button('Get Investor Details')
+    st.title('Investors Analysis')
